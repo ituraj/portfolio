84cb7f6d8e4d32ab311eb45ed8fd6aaff7fc2499
Izabela Turaj <i.turaj@gmail.com>
2019-08-22 10:41:49 +0200

    Updates

---
+++ precache-manifest.5b92cbebb7dacbaa2fa31ef5c501c113.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a397a338debdaf040f6123e8b45ede",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2734aa45467f9eb4b3ff",
+    "url": "/portfolio/static/css/main.efd5e992.chunk.css"
+  },
+  {
+    "revision": "0109dcf54462c4023a9a",
+    "url": "/portfolio/static/js/2.5ab33b4d.chunk.js"
+  },
+  {
+    "revision": "2734aa45467f9eb4b3ff",
+    "url": "/portfolio/static/js/main.37b95354.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "a44490d126f75da69fe80a80934a1df4",
+    "url": "/portfolio/static/media/image1.a44490d1.png"
+  },
+  {
+    "revision": "fb1df665329ca2bda65516764da703f4",
+    "url": "/portfolio/static/media/image2.fb1df665.png"
+  },
+  {
+    "revision": "b64041c9f70d26b65253fc65165c8af4",
+    "url": "/portfolio/static/media/image3.b64041c9.png"
+  },
+  {
+    "revision": "58bbf8c2f881221d0e3b9a4fbe1ff281",
+    "url": "/portfolio/static/media/max.58bbf8c2.svg"
+  },
+  {
+    "revision": "efb1c993e689eb691aae98eb89b9b0bc",
+    "url": "/portfolio/static/media/min.efb1c993.svg"
+  },
+  {
+    "revision": "ff7f83641542d5bbce929e9af7d6e85f",
+    "url": "/portfolio/static/media/project2.ff7f8364.png"
+  },
+  {
+    "revision": "28c17674d2e8a320086b453346f5f351",
+    "url": "/portfolio/static/media/project3.28c17674.png"
+  },
+  {
+    "revision": "112ddf794e8c0acf7b8463a34b74c396",
+    "url": "/portfolio/static/media/resume.112ddf79.pdf"
+  }
+]);
